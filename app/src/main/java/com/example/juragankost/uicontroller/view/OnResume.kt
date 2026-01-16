ecc04796007f962e0fb6d5387210136143358d2c
gibranrafii <gibranrafi21@gmail.com>
2026-01-16 23:27:51 +0700

    membuat fungsi untuk refresh tampilan agar selalu ter refresh

---
+++ app/src/main/java/com/example/juragankost/uicontroller/view/OnResume.kt
@@ -0,0 +1,24 @@
+package com.example.juragankost.uicontroller.view
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+
+// Ini adalah fungsi bantuan (Helper) agar kodingan di halaman lain jadi pendek
+@Composable
+fun OnResume(action: () -> Unit) {
+    val lifecycleOwner = LocalLifecycleOwner.current
+    DisposableEffect(lifecycleOwner) {
+        val observer = LifecycleEventObserver { _, event ->
+            if (event == Lifecycle.Event.ON_RESUME) {
+                action() // Jalankan perintah saat halaman muncul kembali
+            }
+        }
+        lifecycleOwner.lifecycle.addObserver(observer)
+        onDispose {
+            lifecycleOwner.lifecycle.removeObserver(observer)
+        }
+    }
+}
