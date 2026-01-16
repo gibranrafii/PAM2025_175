dd3394ee787a8b6d8bee96880058523d591845dd
gibranrafii <gibranrafi21@gmail.com>
2026-01-16 23:32:03 +0700

    memanggil pengelola halaman nya di main

--- app/src/main/java/com/example/juragankost/MainActivity.kt
+++ app/src/main/java/com/example/juragankost/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.juragankost.ui.theme.JuraganKostTheme
+import com.example.juragankost.uicontroller.route.PengelolaHalaman
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,10 +21,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             JuraganKostTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    // Panggil Navigasi Utama
+                    PengelolaHalaman()
                 }
             }
         }
